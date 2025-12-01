4703753b56ce46f1dfb1585da0f239af75e4ff7e
Shafin <shafinawakes@gmai.com>
2025-12-01 08:39:28 +0600

    extern declaration for memstats

--- syscall.c
+++ syscall.c
@@ -104,6 +104,7 @@ extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
 extern int sys_demo(void);
+extern int sys_memstats(void); //extern declaration for memstats
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
